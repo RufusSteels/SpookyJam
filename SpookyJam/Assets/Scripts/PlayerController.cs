0ecbd15d2577139eebe4daf0b62860b44a893363
RufusSteels <52851612+RufusSteels@users.noreply.github.com>
2021-10-11 13:13:42 +0200

    Stun mechanic added

--- SpookyJam/Assets/Scripts/PlayerController.cs
+++ SpookyJam/Assets/Scripts/PlayerController.cs
@@ -7,22 +7,34 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private GameObject _skullModel;
 
-    private Transform _start;
-    private bool _falling = false;
-    private Vector3 _velocity = Vector3.zero;
-
     [SerializeField]
     [Range(0, 30)]
     private float _speed = 5f;
 
+    private Transform _start;
+    private Vector3 _verticalVelocity = Vector3.zero;
+
+    private bool _falling = false;
+    private bool _stunned = false;
+
+    [SerializeField]
+    private float _stunTime = 1f;
+    private float _stunTimer = 0;
+
+    private GameObject[] _enemies;
+
+    [SerializeField]
+    private AudioSource bonk;
+
     private void Start()
     {
         _start = _skullModel.transform;
+
+        _enemies = GameObject.FindGameObjectsWithTag("Enemy");
     }
 
     void Update()
     {
-
         if (Input.GetButtonDown("Fire1"))
         {
             _falling = true;
@@ -30,13 +42,16 @@ public class PlayerController : MonoBehaviour
 
         Move();
         Fall();
+        Stun();
     }
 
     private void Move()
     {
-        if (!_falling)
+        if (!_falling && !_stunned)
         {
-            this.transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Time.deltaTime * _speed;
+            Vector3 velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Time.deltaTime * _speed;
+            this.transform.position += velocity;
+            this.transform.forward = velocity.normalized;
         }
     }
 
@@ -44,16 +59,45 @@ public class PlayerController : MonoBehaviour
     {
         if (_falling)
         {
-            _skullModel.transform.position += _velocity * Time.deltaTime;
-            _velocity += Physics.gravity * Time.deltaTime;
+            _skullModel.transform.position += _verticalVelocity * Time.deltaTime;
+            _verticalVelocity += Physics.gravity * Time.deltaTime;
 
             if (_skullModel.transform.localPosition.y <= .5f)
             {
                 _falling = false;
-                _skullModel.transform.localPosition = new Vector3(0, 1.5f, 0);
-                _skullModel.transform.localRotation = _start.localRotation;
-                _velocity = Vector3.zero;
+                _stunned = true;
+                _stunTimer = _stunTime;
+                _verticalVelocity = Vector3.zero;
+                EmitNoise();
             }
         }
+    }
+
+    private void Stun()
+    {
+        if (_stunned)
+        {
+            _stunTimer -= Time.deltaTime;
+
+            if(_stunTimer < 0)
+            {
+                _skullModel.transform.localPosition = new Vector3(0, 1.5f, 0);
+                _skullModel.transform.localRotation = _start.localRotation;
+                _stunned = false;
+            }
+        }
+    }
+
+    private void EmitNoise()
+    {
+        bonk.Play();
+
+        //foreach (GameObject e in _enemies)
+        //{
+        //    if(e.TryGetComponent<EnemyController>(out var enemy))
+        //    {
+        //        enemy.Ping(this.transform.position);
+        //    }
+        //}
     }
 }
