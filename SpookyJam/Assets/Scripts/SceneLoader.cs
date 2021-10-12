4e612815fafd97d48639892ad42ce7e277d3fb28
DaffierPear <liam.boesmans@outlook.com>
2021-10-12 15:20:07 +0200

    start

---
+++ SpookyJam/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private int _levelAmount = 3;
+
+    public void Load(int levelIndex)
+    {
+        SceneManager.LoadScene($"Level_{levelIndex}");
+    }
+
+    public void LoadRandom()
+    {
+        int levelIndex = Random.Range(0,_levelAmount);
+        SceneManager.LoadScene($"Level_{levelIndex}");
+    }
+}
