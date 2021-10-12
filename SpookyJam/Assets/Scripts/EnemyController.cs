54e408b51db5caff80f117c9d1efb548ee39118d
Bit <vital.vanhollebeke@hotmail.com>
2021-10-12 21:26:39 +0200

    Level 2 + some enemycontroller changes

--- SpookyJam/Assets/Scripts/EnemyController.cs
+++ SpookyJam/Assets/Scripts/EnemyController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Assets.Scripts;
 using UnityEngine;
 using UnityEngine.AI;
@@ -31,17 +32,24 @@ public class EnemyController : MonoBehaviour
     }
     private void FindNearestFriendly()
     {
+        List<GameObject> friendliesList = new List<GameObject>();
         bool foundFriendly = false;
         Collider[] allObjs = Physics.OverlapSphere(this.transform.position, _lookRadius);
         foreach (var collider in allObjs)
         {
             if (collider.tag == "Friendly")
             {
-                _agent.destination = collider.gameObject.transform.position;
-                this.GetComponent<Wander>().enabled = false;
-                foundFriendly = true;
+                friendliesList.Add(collider.gameObject);
             }
         }
+
+        if (friendliesList.Count > 0)
+        {
+            _agent.destination = friendliesList.First().transform.position;
+            this.GetComponent<Wander>().enabled = false;
+            foundFriendly = true;
+        }
+
         if (!foundFriendly)
         {
             this.GetComponent<Wander>().enabled = true;
